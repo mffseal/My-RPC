428ad94f347ade83aa17147fba6383e31bdda71d
mffseal <mfjdtz@163.com>
2022-07-03 15:07:39 +0800

    [v3.0.7] 完善Nacos服务发现逻辑。

--- rpc-core/src/main/java/top/mffseal/rpc/registry/NacosServiceDiscovery.java
+++ rpc-core/src/main/java/top/mffseal/rpc/registry/NacosServiceDiscovery.java
@@ -26,12 +26,12 @@ public class NacosServiceDiscovery implements ServiceDiscovery {
     public InetSocketAddress lookupService(String serviceName) {
         try {
             // 获取到某个服务的所有提供者列表
-            List<Instance> instances = namingService.getAllInstances(serviceName);
+            List<Instance> instances = NacosUtil.getAllInstances(namingService, serviceName);
             // 目前均采用找到的第一个服务提供者
             // TODO 负载均衡
             Instance instance = instances.get(0);
             return new InetSocketAddress(instance.getIp(), instance.getPort());
-        } catch (NacosException e) {
+        } catch (NacosException | IndexOutOfBoundsException e) {
             log.error("获取服务提供者时发生错误: ", e);
         }
         return null;
