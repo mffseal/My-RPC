b131aedc3cbe0e89cc764155fe8c0c3bc455e7f8
mffseal <mfjdtz@163.com>
2022-07-03 17:02:33 +0800

    [v3.0.8] 单例工厂工具类添加对有参构造函数和工厂静态方法的支持。

--- rpc-common/src/main/java/top/mffseal/rpc/factory/SingletonFactory.java
+++ rpc-common/src/main/java/top/mffseal/rpc/factory/SingletonFactory.java
@@ -1,14 +1,24 @@
 package top.mffseal.rpc.factory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
+ * 通过双重检查锁实现的单例工厂，支持无参构造函数，有参构造函数，工厂类进行对象构造。
+ *
  * @author mffseal
  */
 public class SingletonFactory {
     /**
-     * 记录所有已经实例化过的单例
+     * 记录所有已经实例化过的单例。
      */
     private static final Map<Class<?>, Object> objectMap = new HashMap<>();
     private static final Object lock = new Object();
@@ -20,11 +30,11 @@ public class SingletonFactory {
     }
 
     /**
-     * 双重校验锁实现的单例方法。
+     * 调用类的无参构造函数。
      *
-     * @param clazz 需要创建单例的类
-     * @param <T>   对象类型
-     * @return 单例对象
+     * @param clazz 目标类
+     * @param <T>   目标类型
+     * @return 目标对象
      */
     public static <T> T getInstance(Class<T> clazz) {
         Object instance = objectMap.get(clazz);
@@ -42,5 +52,79 @@ public class SingletonFactory {
             }
         }
         return clazz.cast(instance);
+    }
+
+    /**
+     * 调用类的有参构造函数。
+     *
+     * @param clazz      目标类
+     * @param paramTypes 构造函数参数类型列表
+     * @param args       构造函数参数列表
+     * @param <T>        目标类型
+     * @return 目标对象
+     */
+    public static <T> T getInstance(Class<T> clazz, Class<?>[] paramTypes, Object[] args) {
+        Object instance = objectMap.get(clazz);
+        if (instance == null) {
+            synchronized (lock) {
+                instance = objectMap.get(clazz);
+                if (instance == null) {
+                    try {
+                        Constructor<T> constructor = clazz.getDeclaredConstructor(paramTypes);
+                        instance = constructor.newInstance(args);
+                        objectMap.put(clazz, instance);
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException |
+                             InstantiationException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        return clazz.cast(instance);
+    }
+
+    /**
+     * 通过提供的工厂静态方法实例化类。
+     *
+     * @param clazz      目标类
+     * @param Factory    工厂类
+     * @param methodName 工厂静态方法
+     * @param paramTypes 实例化所需参数类型列表
+     * @param args       实例化所需参数列表
+     * @param <T>        目标类型
+     * @return 目标对象
+     */
+    public static <T> T getInstance(Class<T> clazz, Class<?> Factory, String methodName, Class<?>[] paramTypes, Object[] args) {
+        Object instance = objectMap.get(clazz);
+        if (instance == null) {
+            synchronized (lock) {
+                instance = objectMap.get(clazz);
+                if (instance == null) {
+                    try {
+                        Method method = Factory.getMethod(methodName, paramTypes);
+                        instance = method.invoke(Factory, args);
+                        objectMap.put(clazz, instance);
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        return clazz.cast(instance);
+    }
+
+    /**
+     * 本地测试。
+     * @param args 无用
+     */
+    public static void main(String[] args) {
+        Logger testLog = getInstance(Logger.class, LoggerFactory.class, "getLogger", new Class[]{Class.class}, new Object[]{SingletonFactory.class});
+        testLog.error("123");
+
+        Integer integer = getInstance(Integer.class, new Class[]{int.class}, new Object[]{1});
+        testLog.error("{}", integer);
+
+        List<Integer> list = getInstance(ArrayList.class);
+        testLog.error("{}", list);
     }
 }
